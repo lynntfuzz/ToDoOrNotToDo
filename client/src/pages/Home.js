ef79a25c329679825f04d265556c9843778288c3
Lynn Thompson <lynninla@earthlink.net>
2019-02-08 07:22:33 -0800

    redid skeleton. Complete overhaul.

---
+++ client/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+
+class Home extends Component {
+
+  render() {
+    return <div className="greeting">Hello Habitrons</div>;
+  }
+}
+
+export default Home;
