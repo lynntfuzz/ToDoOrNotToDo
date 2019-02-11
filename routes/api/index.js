429f3f2106860de7b172b91e6f00217096b4cc00
Lynn Thompson <lynninla@earthlink.net>
2019-02-10 22:00:00 -0800

    Added ToDoListItems and added ChallengeListView on home page

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const userRoutes = require("./users");
 const challengeRoutes = require("./challenges");
+const toDoItemRoutes = require("./toDoItems");
 
 // article routes
 router.use("/users", userRoutes);
 router.use("/challenges", challengeRoutes);
+router.use("/toDoItems", toDoItemRoutes);
 
 module.exports = router;
