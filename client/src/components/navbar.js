65fa7c8028dc4a8de3287b39d0fbe6bfda32a7ac
DangerGreen <r.malikgreen@gmail.com>
2019-02-11 20:03:33 -0800

    basic navbar

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Nav, NavItem, Dropdown, DropdownItem, DropdownToggle, DropdownMenu, NavLink} from 'reactstrap'   
 
-
 export default class Navigation extends React.Component {
   constructor(props) {
     super(props);
