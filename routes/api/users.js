a23f2ae2db01707a0fb49aad61ec2ede1c1cc8b3
Jobi Mathai <jobi.mathai@gmail.com>
2019-02-16 11:46:06 -0800

    login functionality, passport doesn't work

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
 const usersController = require("../../controllers/usersController");
+const passport = require("../../client/src/config/passport");
 
 // Matches with "/api/users"
 router.route("/")
   .get(usersController.findAll)
   .post(usersController.signUpUser)
-  .post(usersController.loginUser)
 
 module.exports = router;
